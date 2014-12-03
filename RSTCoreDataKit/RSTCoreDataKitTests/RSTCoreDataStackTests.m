186c54bcf9ed14444e918f3272124bd863d58d07
Jesse Squires <jsquires@rosettastone.com>
2014-12-03 12:32:21 -0800

    begin stack tests

---
+++ RSTCoreDataKit/RSTCoreDataKitTests/RSTCoreDataStackTests.m
@@ -0,0 +1,44 @@
+//
+//  Rosetta Stone
+//  http://product.rosettastone.com/news/
+//
+//
+//  Documentation
+//  http://cocoadocs.org/docsets/RSTCoreDataKit
+//
+//
+//  GitHub
+//  https://github.com/rosettastone/RSTCoreDataKit
+//
+//
+//  License
+//  Copyright (c) 2014 Rosetta Stone
+//  Released under a BSD license: http://opensource.org/licenses/BSD-3-Clause
+//
+
+#import "RSTCoreDataKitTestCase.h"
+
+
+@interface RSTCoreDataStackTests : RSTCoreDataKitTestCase
+
+@end
+
+
+@implementation RSTCoreDataStackTests
+
+- (void)setUp
+{
+    [super setUp];
+}
+
+- (void)tearDown
+{
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+@end
